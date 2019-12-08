ec2c128b448b53c54a0c6bcba1ff54688af15c4c
ryancharlessmith <ryan-smith@comcast.net>
2019-12-08 18:27:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+  
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( var i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+
+function countDown(start){
+
+    while(start >= 0){
+        console.log(start)
+        start --
+    }
+}
+
