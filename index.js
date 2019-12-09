8fca55030c2158193a5b5506e97ae9fe197660ea
mrh9ud <mrh9ud@virginia.edu>
2019-12-08 19:48:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+// }
+
+// const gifts = ['teddy bear', 'drone', 'doll'];
+
+// function wrapGifts(gift) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     }
+//     return gifts;
+// }
+
+function writeCards(array, name) {
+    let messages = []
+    for (let i = 0; i < array.length; i++) {
+        messages.push(`Thank you, ${array[i]}, for the wonderful ${name} gift!`);
+    }
+    return messages;
+}
+
+function countDown(positiveInteger) {
+    while (positiveInteger > 0) {
+        console.log(positiveInteger);
+        positiveInteger -= 1;
+    }
+    console.log(positiveInteger);
+}
